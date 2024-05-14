dc521d1c7f38f2fca450ab23e036ca6a2997e3a5
BaselAhmed <basel20ahmed@gmail.com>
2024-05-14 15:31:32 +0300

    added .gitattributes

---
+++ model-loader/Model.h
@@ -0,0 +1,25 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <math.h>
+#include <time.h>
+
+#include <GL/freeglut.h>
+#include <GL/glext.h>
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+
+#define WIDTH 600
+#define HEIGHT 600
+
+#define INTERVAL 15
+
+class Model {
+
+};
+
+
+#endif
